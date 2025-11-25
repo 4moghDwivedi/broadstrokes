9094a8ad1cace948d490350b3460f13c8e4af6b9
Amogh Dwivedi <158484445+4moghDwivedi@users.noreply.github.com>
2025-11-24 22:02:50 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+let brushes = [];
+
+let numParticles = random(10, 300);
+
+let backCol = 0;
+
+let count = 0;
+function setup() {
+  // Create a canvas that fills the entire browser window
+  createCanvas(windowWidth, windowHeight);
+  background(backCol);
+
+  for (let i = 0; i < numParticles; i++)
+    {
+      brushes.push(new particle(width/2, height/2, random(1, 7), 7));
+    }
+
+
+}
+
+function draw() {
+
+
+for (let i = 0; i < numParticles; i++)
+  {
+    brushes[i].display();
+    brushes[i].update();
+  }
+
+  count += 1;
+
+  if (count % (60*15) == 0)
+    {
+
+      backCol = 1 - backCol;
+      background(backCol*255); 
+
+      for (let i = 0; i < numParticles; i++)
+        {
+          brushes.pop();
+        }
+
+        numParticles = random(50, 300);
+
+        for (let i = 0; i < numParticles; i++)
+          {
+            brushes.push(new particle(width/2, height/2,  7, 7));
+          }
+    }
+
+}
